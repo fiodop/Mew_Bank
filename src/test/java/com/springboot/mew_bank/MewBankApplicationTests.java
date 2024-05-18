58f78bda1520c06d6636d8c6adbac0d86adbe3cb
artem <artemklucarov666@gmail.com>
2024-05-18 15:14:01 +0300

    improved problems with controller method money amount

---
+++ src/test/java/com/springboot/mew_bank/MewBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.mew_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MewBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
